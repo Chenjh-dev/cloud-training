798b6848521e34a8f25ea3a3a8b3c807feae3c7d
To-be-a-pig <75511126+To-be-a-pig@users.noreply.github.com>
2023-07-19 16:25:57 +0800

    第一次提交

---
+++ cloud-demo/user-service/src/test/java/com/hui/cn/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hui.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
